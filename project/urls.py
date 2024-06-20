3c9c2b7d9b64e04892d5492fdbce276d94cd4b49
John Doe <crijacarla@gmail.com>
2024-06-20 20:16:01 -0300

    Criar rota para método post

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from app import view
+from app import views
 
 urlpatterns = [
-    path('login', view.login, name='create_login'),
+    path('create_user', views.user, name='test_user')
+  # path('login', views.login, name='create_login'),
 ]
