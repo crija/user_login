7dbc7750240790944209e4729bd100be3ccb8d69
John Doe <crijacarla@gmail.com>
2024-07-02 11:26:24 -0300

    Rename

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('create_user', views.user, name='test_user')
-  # path('login', views.login, name='create_login'),
+    path('register', views.user, name='register')
 ]
